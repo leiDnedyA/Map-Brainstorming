ddfaa76da54de074e577dbedf38edb3c3c3a2e62
Ayden <aydendiel@gmail.com>
2023-01-19 14:01:21 -0500

    created and tested WIP coordinate range

--- public/src/coordinates.js
+++ public/src/coordinates.js
@@ -1,5 +1,40 @@
 
 /**
+ * function checkValidCoords
+ * 
+ * takes a pair of lat/long coordinates and makes sure that they're within the 
+ * bounds of the lng/lat system. The longitude is restricted to [-85, 85]. If the 
+ * latitude is outside of the proper boundaries, it will simply be set to the corresponding
+ * boundary += the distance from it's current value to the max/min that it's breaking.
+ * 
+ * return value: [lng, lat]
+ * 
+ */
+
+function checkValidCoords(lng, lat){
+
+    let validLng = lng;
+    let validLat = lat;
+
+    if(lat < -180){
+        const dif = -180 - lat;
+        validLat = 180 - dif;
+    }else if(lat > 180){
+        const dif = lat - 180;
+        validLat = -180 + dif;
+    }
+
+    if(lng < -85){
+        validLng = -85;
+    }else if(lng > 85){
+        validLng = 85;
+    }
+
+    return [validLng, validLat];
+
+}
+
+/**
  * function getCoordinateBounds(long, lat, range)
  * 
  * generates a coordinate range of the following format, 
@@ -7,25 +42,51 @@
  * 
  * returns object:
  * {
- *  ne_lat: 0,
-    ne_lng: 0,
-    sw_lat: 0,
-    sw_lng: 0
+ *  ne: [lng, lat],
+ *  sw: [lng, lat]
  * }
  * 
  */
 
 function getCoordinateBounds(lng, lat, range = 5) {
 
+    console.log(checkValidCoords(lng, lat))
+
     const bounds = {
-        ne_lat: lat + range,
-        ne_lng: lng + range,
-        sw_lat: lat - range,
-        sw_lng: lng - range
+        ne: [lng + range, lat + range],
+        sw: [lng - range, lat - range]
     }
 
-    //for loop that checks if coords are valid and adjusts them if need be
+    return bounds;
 
 }
 
-export default { getCoordinateBounds }
+/**
+ * Like getCoordinateBounds, but adjusted for coords outside of normal lat/lng range
+ * 
+ */
+function getCheckedCoordinateBounds(lng, lat, range = 1) {
+
+    console.log(checkValidCoords(lng, lat))
+
+    const bounds = {
+        ne: checkValidCoords(lng + range, lat + range),
+        sw: checkValidCoords(lng - range, lat - range)
+    }
+
+    return bounds;
+
+}
+
+/**
+ * function getBoundingBox(lng, lat, range)
+ * 
+ * generates a full bounding box from coord+range to coord-range
+ * 
+ */
+
+function getBoundingBox(lng, lat, range = .1){
+
+}
+
+export { getCoordinateBounds }
